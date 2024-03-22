ae06df55fd679703d8507447d883396689fdf91e
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-03-23 01:21:31 +0300

    fix(profession circle) refactor style and render elements

--- src/features/profession/item/ProfessionItem.tsx
+++ src/features/profession/item/ProfessionItem.tsx
@@ -4,30 +4,40 @@ import {DataType} from '../../../common';
 
 import s from './ProffesionItem.module.css';
 
-interface IProfessionItem {
-    profession: DataType
-    index: number
-    total: number
+interface IProfessionItem2 {
+  profession: DataType
+  index: number
+  total: number
 }
 
-export const ProfessionItem: FC<IProfessionItem> = ({
+export const ProfessionItem:FC<IProfessionItem2> = ({
   profession,
   index,
   total,
 }) => {
 
   const angle = (index / total) * Math.PI * 2;
-  const radius = 200;
+  const radius = 170;
+  const radiusItem = -40;
 
-  const left = 250 + Math.cos(angle) * radius;
-  const top = 250 + Math.sin(angle) * radius;
+  const left = 720 + Math.cos(angle) * radius;
+  const top = 345 + Math.sin(angle) * radius;
+
+  const leftItem = 13 + Math.cos(angle) * radiusItem;
+  const topItem = 1 + Math.sin(angle) * radiusItem;
 
   return (
-    <div
-      className={s.professionItem}
-      style={{ left: `${left}px`, top: `${top}px` }}
-    >
+    <div className={s.professionItem}
+      style={{
+        position: 'absolute',
+        left: `${left}px`,
+        top: `${top}px`,
+      }}>
       {profession.name}
+      <div className={s.circleItem} style={{
+        left: `${leftItem}px`,
+        top: `${topItem}px`,
+      }}/>
     </div>
   );
 };
