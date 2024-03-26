94d14fa6b0bb56fd6618913d77ebcc6c86665d08
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-03-27 00:16:14 +0300

    fix(uniqueSkillArray) changing the logic of array assembly

--- src/common/utils/uniqueSkillArray.ts
+++ src/common/utils/uniqueSkillArray.ts
@@ -3,14 +3,14 @@ import {DataType} from '../data/DataSet.ts';
 /**
  * converting the "data" array to calculate the unique values of the "mainSkills" and "otherSkills" arrays
  * @param dataArray
- * @param skillType
  * @return uniqueSkillsSet
  */
-export const uniqueSkillArray = (dataArray: DataType[], skillType: 'mainSkills' | 'otherSkills'): string[] => {
+export const uniqueSkillArray = (dataArray: DataType[]): string[] => {
   const allSkills: string[] = dataArray.reduce((accumulator: string[], currentValue: DataType) => {
-    return accumulator.concat(currentValue[skillType]);
+    return accumulator.concat(currentValue.mainSkills, currentValue.otherSkills);
   }, []);
 
+  // Filter out duplicates
   const uniqueSkillsSet: Set<string> = new Set(allSkills);
   return [...uniqueSkillsSet];
 };
